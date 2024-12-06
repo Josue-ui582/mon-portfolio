024ea5d5d6e36c34f1e37d7a7a789e7541db6157
JoshuaDev <hounhouijosue1@gmail.com>
2024-12-06 14:18:03 +0100

    adding new project. Fashion monopage web site project

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -19,7 +19,7 @@ export function Navigation() {
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
-            <p className="text-xl font-bold">portfolio</p>
+            <p className="text-xl font-bold uppercase">portfolio</p>
 
           <div className="hidden md:flex items-center space-x-8">
             <Link href="/" className="hover:text-primary transition-colors">
